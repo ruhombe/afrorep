e22da25adce24e1e73094cd3696860e5b59f7688
alexiscyber14 <142sledgehammer14@gmail.com>
2023-11-01 22:14:18 +0100

    add register and login pt1

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db import models
+from django.utils.html import mark_safe
+from mptt.models import MPTTModel, TreeForeignKey
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.html import format_html
+from django.urls import reverse
+from django.db.models.signals import pre_save, post_delete
+from django.dispatch import receiver
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Profiles(models.Model):
+    user =  models.OneToOneField(User, on_delete=models.CASCADE,  null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    phone = models.CharField(max_length=200, null=True, blank=True)
+    address = models.TextField(max_length=200,null=True, blank=True)
+    def __str__(self):
+        return self.name
--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.loginUser, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
 
 ]
